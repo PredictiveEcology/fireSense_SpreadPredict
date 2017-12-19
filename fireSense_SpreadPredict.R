b376c94f060654ad6db14cc421053977f6b63b63
JM <jean.d.marchal@gmail.com>
2017-12-19 10:07:07 -0500

    Not falling back anymore to the simList env to search for objects

--- fireSense_SpreadPredict.R
+++ fireSense_SpreadPredict.R
@@ -168,7 +168,7 @@ fireSense_SpreadPredictRun <- function(sim)
   
   if (s <- sum(missing))
     stop(paste0(moduleName, "> '", allxy[missing][1L], "'", if (s > 1) paste0(" (and ", s-1L, " other", if (s>2) "s", ")"),
-                " not found in data objects nor in the simList environment."))
+                " not found in data objects."))
 
   sim$fireSense_SpreadPredicted[as.character(currentTime)] <- list(
     mget(allxy, envir = envData, inherits = FALSE) %>%
