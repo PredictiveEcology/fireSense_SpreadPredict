5f503cd74103fcef109e5e9dd27fa108c8a6c03a
Alex Chubaty <alex.chubaty@gmail.com>
2020-04-13 12:08:45 -0600

    misc cleanup (Rmd and README)

---
+++ README.md
@@ -0,0 +1,3 @@
+# fireSense_SpreadPredict
+
+See [`fireSense_SpreadPredict.html`](fireSense_SpreadPredict.html) for information about this module.
--- fireSense_SpreadPredict.R
+++ fireSense_SpreadPredict.R
@@ -6,7 +6,7 @@ defineModule(sim, list(
   keywords = c("fire spread", "fireSense", "predict"),
   authors = c(person("Jean", "Marchal", email = "jean.d.marchal@gmail.com", role = c("aut", "cre"))),
   childModules = character(),
-  version = list(SpaDES.core = "0.1.0", fireSense_SpreadPredict = "0.0.1"),
+  version = list(fireSense_SpreadPredict = "0.0.1", SpaDES.core = "0.1.0"),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
